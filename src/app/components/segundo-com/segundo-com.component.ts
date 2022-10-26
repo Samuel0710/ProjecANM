027abbac172f7c59a2af5ac3bbd5023e8c44aaa4
Samuel0710 <deadpoolscarlet@gmail.com>
2022-10-26 15:02:28 -0300

    alteraçoes direto na lista e ajustando erro de exibiçao

--- src/app/components/segundo-com/segundo-com.component.ts
+++ src/app/components/segundo-com/segundo-com.component.ts
@@ -1,3 +1,5 @@
+import { Data } from './../../model/Data';
+
 import { Component, OnInit } from '@angular/core';
 import { Animes } from 'src/app/model/animeMo.model';
 import { ServerService } from 'src/app/server/server.service';
@@ -8,22 +10,31 @@ import { ServerService } from 'src/app/server/server.service';
   styleUrls: ['./segundo-com.component.scss'],
 })
 export class SegundoComComponent implements OnInit {
-  animes: any;
-  erro: any;
 
-  constructor(private crudServer: ServerService) { 
+  animes:Animes[]=[];
+  erro: any;
+  data: Data[]=[];
+
+  constructor(private crudServer: ServerService) {
     this.getter();
   }
 
   ngOnInit() {}
 getter(){
   this.crudServer.getAnime().subscribe(
-    (data: Animes)=>{this.animes = data;
-      console.log('a variavel que prenchemos', this.animes);
+    (data: Animes)=>{this.animes.push({
+      paginition: data.paginition,
+      data: data.data
+    });
+    this.data=this.animes[0].data;
+    console.log ('parametro data',this.data);
+      console.log('lista', this.animes);
+      console.log('lista data',data);
     },
     error=>{
       this.erro= error;
       console.error=(error);
     });
+
 }
 }
--- src/app/server/server.service.ts
+++ src/app/server/server.service.ts
@@ -1,4 +1,3 @@
-import { Animes } from 'src/app/model/animeMo.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -11,7 +10,5 @@ export class ServerService {
   constructor(private http: HttpClient) { }
     public getAnime(): Observable<any>{
      return this.http.get(`https://api.jikan.moe/v4/anime`);
-     }
-     
-  
+     };
 }
